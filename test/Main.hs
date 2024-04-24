14bd501ca3bbc64f72dac3d06bd2d5b40b64e320
andrewchawk <noreply@spam.bin>
2024-04-23 22:47:52 -0500

    Trying to improve readability and maintainability, I split up the main test case function.

--- test/Main.hs
+++ test/Main.hs
@@ -28,49 +28,76 @@ main = maybe exitSuccess (\t -> printFailMsg t >> exitFailure) equalChkResults
     where printError = putStrLn . ("- " ++)
   chkEquality (a,b,c) = checkExpEquality a b c
   equalChkResults = sequenceEqualityErrors $ map chkEquality $
-    [("Limit of infinity as x approaches infinity",
-      Ap1 (Limit "x" Infinity) Infinity,
-      Just Infinity),
-     ("Limit of x as x approaches n",
-      Ap1 (Limit "x" $ Variable "n") (Variable "x"),
-      Just $ Variable "n"),
-     ("Dividing by zero zero",
+    testsForDiff ++
+    testsForLimit ++
+    testsForLambda ++
+    testsForExpRatio ++
+    testsForNormalForm ++
+     [("Dividing by zero zero",
       (\z -> Ap2 Quotient z z) $ ExpRatio $ 0 % 1,
       Nothing),
      ("Square of square root of -1",
       Ap2 (Flip Exponent) (ExpRatio $ 2 % 1) $ Ap2 Exponent (ExpRatio $ (-1) % 1) $ ExpRatio $ 1 % 2,
       Just $ ExpRatio $ (-1) % 1),
-     ("Simple lambda with unused variable",
-      Ap1 (Lambda "x" Infinity) $ Ap1 Negate Infinity,
-      Just Infinity),
-     ("Potentially tricky lambda substitution",
-      Ap1 (Lambda "x" $ Ap1 (Lambda "z" $ Variable "g") Infinity) $ Ap1 Negate Infinity,
-      Just $ Variable "g"),
-     ("Derivative of the deriviative of sin x",
-      Ap1 (Diff "x") $ Ap1 (Diff "x") $ Ap1 Sin $ Variable "x",
-      Just $ Ap1 Negate $ Ap1 Sin $ Variable "x"),
-     ("Derivative of the sine of an unrelated variable",
-      Ap1 (Diff "z") $ Ap1 Sin $ Variable "x",
-      Just $ Ap1 (Diff "z") $ Ap1 Sin $ Variable "x"),
      ("Adding infinity and negative infinity",
       Ap2 Sum Infinity $ Ap1 Negate Infinity,
-      Just $ ExpRatio $ 0 % 1),
-     ("Normal form conversion of x * 2/1",
-      Ap2 Product (Variable "x") (ExpRatio $ 2 % 1),
-      Just $ Ap2 Product (ExpRatio $ 2 % 1) (Variable "x")),
-     (let (a,b) = ((2407620, 45672), (28467, 329057)) in
-      ("Basic ExpRatio addition",
-       Ap2 Sum (ExpRatio $ fst a % snd a) (ExpRatio $ fst b % snd b),
-       Just $ ExpRatio $ (fst a % snd a) + (fst b % snd b))),
-     (let (a,b) = ((2407620, 45672), (28467, 329057)) in
-      ("Basic ExpRatio subtraction",
-       Ap2 Sum (ExpRatio $ fst a % snd a) (Ap1 Negate $ ExpRatio $ fst b % snd b),
-       Just $ ExpRatio $ (fst a % snd a) - (fst b % snd b))),
-     (let (a,b) = ((2407620, 45672), (28467, 329057)) in
-      ("Basic ExpRatio multiplication",
-       Ap2 Product (ExpRatio $ fst a % snd a) (ExpRatio $ fst b % snd b),
-       Just $ ExpRatio $ (fst a % snd a) * (fst b % snd b))),
-     (let (a,b) = ((2407620, 45672), (28467, 329057)) in
-      ("Basic ExpRatio division",
-       Ap2 Quotient (ExpRatio $ fst a % snd a) (ExpRatio $ fst b % snd b),
-       Just $ ExpRatio $ (fst a % snd a) / (fst b % snd b)))]
+      Just $ ExpRatio $ 0 % 1)]
+
+-- | Values of this type can be used as input for 'checkExpEquality' and,
+-- therefore, are test cases.
+type TestCase = (String, Expression, Maybe Expression)
+
+-- | This value is a list of test cases which mostly pertain to 'Diff'.
+testsForDiff :: [TestCase]
+testsForDiff =
+  [("Derivative of the deriviative of sin x",
+    Ap1 (Diff "x") $ Ap1 (Diff "x") $ Ap1 Sin $ Variable "x",
+    Just $ Ap1 Negate $ Ap1 Sin $ Variable "x"),
+   ("Derivative of the sine of an unrelated variable",
+    Ap1 (Diff "z") $ Ap1 Sin $ Variable "x",
+    Just $ Ap1 (Diff "z") $ Ap1 Sin $ Variable "x")]
+
+-- | This value is a list of test cases which mostly pertain to 'Limit'.
+testsForLimit :: [TestCase]
+testsForLimit =
+  [("Limit of infinity as x approaches infinity",
+    Ap1 (Limit "x" Infinity) Infinity,
+    Just Infinity),
+   ("Limit of x as x approaches n",
+    Ap1 (Limit "x" $ Variable "n") (Variable "x"),
+    Just $ Variable "n")]
+
+-- | This value is a list of test cases which mostly pertain to 'Lambda'.
+testsForLambda :: [TestCase]
+testsForLambda =
+  [("Simple lambda with unused variable",
+    Ap1 (Lambda "x" Infinity) $ Ap1 Negate Infinity,
+    Just Infinity),
+   ("Potentially tricky lambda substitution",
+    Ap1 (Lambda "x" $ Ap1 (Lambda "z" $ Variable "g") Infinity) $ Ap1 Negate Infinity,
+    Just $ Variable "g")]
+
+-- | This value is a list of test cases which mostly pertain to 'ExpRatio'.
+testsForExpRatio :: [TestCase]
+testsForExpRatio =
+  [("Basic ExpRatio addition",
+    Ap2 Sum (ExpRatio $ fst a % snd a) (ExpRatio $ fst b % snd b),
+    Just $ ExpRatio $ (fst a % snd a) + (fst b % snd b)),
+   ("Basic ExpRatio subtraction",
+    Ap2 Sum (ExpRatio $ fst a % snd a) (Ap1 Negate $ ExpRatio $ fst b % snd b),
+    Just $ ExpRatio $ (fst a % snd a) - (fst b % snd b)),
+   ("Basic ExpRatio multiplication",
+    Ap2 Product (ExpRatio $ fst a % snd a) (ExpRatio $ fst b % snd b),
+    Just $ ExpRatio $ (fst a % snd a) * (fst b % snd b)),
+   ("Basic ExpRatio division",
+    Ap2 Quotient (ExpRatio $ fst a % snd a) (ExpRatio $ fst b % snd b),
+    Just $ ExpRatio $ (fst a % snd a) / (fst b % snd b))]
+  where (a,b) = ((2407620, 45672), (28467, 329057))
+
+-- | This value is a list of test cases which mostly pertain to the process of
+-- converting to normal form.
+testsForNormalForm :: [TestCase]
+testsForNormalForm =
+  [("Normal form conversion of x * 2/1",
+    Ap2 Product (Variable "x") (ExpRatio $ 2 % 1),
+    Just $ Ap2 Product (ExpRatio $ 2 % 1) (Variable "x"))]
